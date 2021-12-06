39679ee8667054f7d259521df4681f16f7d9742b
pbl.gllgs <pbl.gllgs@gmail.com>
2021-12-06 12:29:57 -0300

    updates role

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,49 +1,64 @@
 /* ruta /api/usuarios */
 
-const { Router } = require('express');
-const { check } = require('express-validator');
+const { Router } = require("express");
+const { check } = require("express-validator");
 
-const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario, cambiaEstadoUsuario } = require('../controllers/usuarios');
-const { validarCampos } = require('../middleware/validar-campos');
-const { validarJWT } = require('../middleware/validar-jwt');
+const {
+  getUsuarios,
+  crearUsuario,
+  actualizarUsuario,
+  borrarUsuario,
+  cambiaEstadoUsuario,
+} = require("../controllers/usuarios");
+const { validarCampos } = require("../middleware/validar-campos");
+const { validarJWT, validarROLE,validarROLEPropio } = require("../middleware/validar-jwt");
 
 const router = Router();
 
-
 //rutas
-router.get('/',validarJWT, getUsuarios );
+router.get(
+    "/", 
+    [
+        validarJWT, validarROLE
+    ],
+    getUsuarios);
 
 router.post(
-    '/new',
-    [
-        check('nombre','El nombre es obligatorio').not().isEmpty(),
-        check('password','El password es obligatorio').not().isEmpty(),
-        check('email','El email es obligatorio').isEmail(),
-        validarCampos,
-        
-    ],
-    crearUsuario
+  "/new",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("password", "El password es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    validarCampos,
+  ],
+  crearUsuario
 );
 
 router.put(
-    '/:id',
-    [
-        validarJWT,
-        check('nombre','El nombre es obligatorio').not().isEmpty(),
-        check('email','El email es obligatorio').isEmail(),
-        check('role','El role es obligatorio').not().isEmpty(),
-        validarCampos
-    ],
-    actualizarUsuario
+  "/:id",
+  [
+    validarJWT,
+    validarROLEPropio,
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("role", "El role es obligatorio").not().isEmpty(),
+    validarCampos,
+  ],
+  actualizarUsuario
 );
 
-router.delete('/:id',validarJWT,borrarUsuario);
+router.delete(
+    "/:id", 
+    [
+        validarJWT, validarROLE
+    ], 
+    borrarUsuario);
 
-router.post('/:id',validarJWT,cambiaEstadoUsuario);
-
-
-
-
-
+router.post(
+    "/:id",
+    [
+        validarJWT, validarROLE
+    ], 
+    cambiaEstadoUsuario);
 
 module.exports = router;
